70494a96c3333d96dd0b0208eb011df00a82fd94
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-22 14:17:55 +1200

    Started on routes

---
+++ drag.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import sqlite3
+
+app = Flask(__name__)
+@app.route("/home")
+def home():
+
+@app.route("/seasons")
+def seasons():
+
+@app.route("/season_info")
+def season_info():
+
+@app.route("/drag_queens")
+def drag_queens():
+    SELECT * FROM Drag_Queens
+
+@app.route("/drag_queen_info")
+def drag_queen_info():
+
+@app.route("/episode_info")
+def episode_info():
