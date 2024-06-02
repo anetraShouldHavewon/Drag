2eecbfb3b72925d561e294f973d2d2b06172408a
anetraShouldHavewon <tramdang628@gmail.com>
2024-06-03 02:20:48 +1200

    Added drag queens page

--- drag.py
+++ drag.py
@@ -41,15 +41,25 @@ def season_info():
 
 @app.route("/drag_queens")
 def drag_queens():
-    return render_template("drag_queens.html", title = "Drag Queens")
+    # INI --> get the most recent season name/id
+    # Getting the names of the queens from the most recent season in a list
+    drag_queens_info = sql(False, "SELECT Drag_Queen_Season.drag_queen_id, Drag_Queens.name FROM Drag_Queen_Season JOIN Drag_Queens ON Drag_Queen_Season.drag_queen_id = Drag_Queens.id WHERE Drag_Queen_Season.season_id = 1", None)
+    drag_queen_ids = []
+    drag_queen_names = []
+    for queen in range(len(drag_queens_info)):
+        drag_queen_ids.append(drag_queens_info[queen][0])
+        drag_queen_names.append(drag_queens_info[queen][1])
+        
+    return render_template("drag_queens.html", drag_queen_ids = drag_queen_ids, drag_queen_names = drag_queen_names, title = "Drag Queens")
 
 @app.route("/drag_queen_info/<int:id>")
 def drag_queen_info(id):
     drag_queen_info = sql(True, "SELECT * FROM Drag_Queens WHERE id = ?", id)
     name = drag_queen_info[1]
+    specialty_skills = drag_queen_info[2]
     city = drag_queen_info[4]
     age = drag_queen_info[5]
-    return render_template("drag_queen_info.html", name = name, city = city, age = age, drag_queen = drag_queen_info, title = "Drag Queen Information")
+    return render_template("drag_queen_info.html", name = name, specialty_skills = specialty_skills, city = city, age = age, drag_queen = drag_queen_info, title = "Drag Queen Information")
 
 @app.route("/episode_info")
 def episode_info():
--- test.py
+++ test.py
@@ -9,12 +9,12 @@ def sql(fetchone, query, constraint):
         if fetchone is True:
             result = cursor.execute(query).fetchone()
         if fetchone is False: # if the query needs a fetchall
-            results = cursor.execute(query).fetchall()
+            result = cursor.execute(query).fetchall()
     else:
         if fetchone is True:
             result = cursor.execute(query, (constraint,)).fetchone()
         if fetchone is False: # if the query needs a fetchall
-            results = cursor.execute(query, (constraint,)).fetchall()
+            result = cursor.execute(query, (constraint,)).fetchall()
 
     return result
 
@@ -23,4 +23,8 @@ for id in range(1,5):
         drag_name = sql(True, "SELECT name FROM Drag_Queens WHERE id = ?", id)[0]
         drag_queen_names.append(drag_name)
 
+x = sql(False, "SELECT Drag_Queens.name FROM Drag_Queen_Season JOIN Drag_Queens ON Drag_Queen_Season.drag_queen_id = Drag_Queens.id WHERE Drag_Queen_Season.season_id = 1", None)
+drag_queen_names = []
+for queen in range(len(x)):
+    drag_queen_names.append(x[queen][0])
 print(drag_queen_names)
